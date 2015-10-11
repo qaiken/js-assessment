c75d20d544f14b658b03d1b4d256bc8a99c53371
Quinton Aiken <quintonlaiken@gmail.com>
2015-10-11 00:20:22 -0700

    complete async

--- app/async.js
+++ app/async.js
@@ -1,11 +1,35 @@
 exports = (typeof window === 'undefined') ? global : window;
 
+
+
 exports.asyncAnswers = {
   async : function(value) {
-
+    return new Promise(function(resolve, reject) {
+      resolve(value);
+    });
   },
 
   manipulateRemoteData : function(url) {
+    var xmlhttp = new XMLHttpRequest();
+
+    var getNames = function(data) {
+      return data.people.map(function(person) {
+        return person.name;
+      }).sort();
+    };
+
+    return new Promise(function(resolve, reject) {
+      xmlhttp.onreadystatechange = function() {
+        if ( xmlhttp.readyState === 4 && xmlhttp.status === 200 ) {
+          resolve(JSON.parse(xmlhttp.responseText));
+        }
+      };
+      xmlhttp.open('GET', url, true);
+      xmlhttp.send();
+    })
+    .then(function(data) {
+      return getNames(data);
+    });
 
   }
 };
