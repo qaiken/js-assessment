4aa45eb7bcec37727f30e62933862256a043bb02
Quinton Aiken <quintonlaiken@gmail.com>
2015-10-11 16:32:19 -0700

    complete regular expressions

--- app/regex.js
+++ app/regex.js
@@ -2,25 +2,25 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.regexAnswers = {
   containsNumber : function(str) {
-
+    return str.search(/\d/g) !== -1;
   },
 
   containsRepeatingLetter : function(str) {
-
+    return str.toLowerCase().search(/([a-z])\1+/g) !== -1;
   },
 
   endsWithVowel : function(str) {
-
+    return str.toLowerCase().search(/[aeiou]$/) !== -1;
   },
 
   captureThreeNumbers : function(str) {
-
+    var match = str.match(/\d{3}/);
+    return match ? match[0] : false;
   },
-
   matchesPattern : function(str) {
-
+    return str.search(/^\d{3}-\d{3}-\d{4}$/) !== -1;
   },
   isUSD : function(str) {
-
+    return str.search(/^\$(((\d{1,3},)?(\d{3},)+\d{3})|(\d{1,3}))(\.\d{2})?$/) !== -1;
   }
 };
