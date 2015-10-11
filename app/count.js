e9239b8f3d01201c76cac4d0840ea5fe6719197e
Quinton Aiken <quintonlaiken@gmail.com>
2015-10-11 00:41:53 -0700

    complete count

--- app/count.js
+++ app/count.js
@@ -2,6 +2,22 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.countAnswers =  {
   count : function (start, end) {
+    var i = start;
 
+    console.log(i);
+
+    var interval = setInterval(function() {
+      console.log(++i);
+
+      if( i === end ) {
+        clearInterval(interval);
+      }
+    }, 100);
+
+    return {
+      cancel: function() {
+        clearInterval(interval);
+      }
+    };
   }
 };
